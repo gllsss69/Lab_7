21001cd130803f37cfa753c92c970f9480da14df
gllsss69 <maksympetyk08@gmail.com>
2026-02-19 20:51:19 +0200

    Завдання 2. Код виправлено

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace Exercise_2
 {
+   
+    public class BankTerminal
+    {
+
+        public event Action<int> OnMoneyWithDraw;
+
+        public void WithDraw(int amount)
+        {
+            Console.WriteLine($"Термінал видає {amount} грн...");
+            OnMoneyWithDraw.Invoke(amount);
+
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            BankTerminal terminal = new BankTerminal();
+            terminal.OnMoneyWithDraw += amount => Console.WriteLine($"На ваш рахунок начислено {amount} грн.");
+            //terminal.OnMoneyWithDraw = null;
+            terminal.WithDraw(100);
         }
     }
 }
