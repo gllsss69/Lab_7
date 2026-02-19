8a8136ac4a693cbec8c495e68828600c5c692b6b
gllsss69 <maksympetyk08@gmail.com>
2026-02-19 21:10:47 +0200

    Завдання 3

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+           
+            Func<double, double> discountCalculator = null;
+
+           
+            discountCalculator += price => price * 0.90;
+            discountCalculator += price => price * 0.95;
+            discountCalculator += price => price - 100;
+
+           
+          
+            double currentPrice = 1000;
+            Console.WriteLine($"Початкова ціна: {currentPrice} грн");
+
+           
+            Delegate[] delegates = discountCalculator.GetInvocationList();
+
+          
+            foreach (Func<double, double> discountMethod in delegates)
+            {
+               
+                currentPrice = discountMethod(currentPrice);
+
+                Console.WriteLine($"Ціна після застосування чергової знижки: {currentPrice} грн");
+            }
+
+            Console.WriteLine($"\nФінальна ціна до оплати: {currentPrice} грн");
         }
     }
 }
